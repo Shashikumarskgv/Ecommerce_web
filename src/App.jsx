282dcc36577dacab7536de70ec080809780caab9
Shashi_Kumar <shashikumarskgv@gmail.com>
2025-01-05 23:28:50 +0530

    Fixed file path issues and completed build

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Products from "./components/Products/Products";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import TopProducts from "./components/TopProducts/TopProducts";
 import Banner from "./components/Banner/Banner";
 import Subscribe from "./components/Subscribe/Subscribe";
